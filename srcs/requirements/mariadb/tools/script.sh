b7f17c27b77b5cd1872898efcebc584f478f54f1
root <eduardo.mpsilva1411@gmail.com>
2024-05-20 17:31:48 +0100

    so close

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,44 +1,18 @@
 #!/bin/sh
+service mysql start 
 
-mysql_install_db
+# CREATE USER #
+echo "CREATE USER '$BDD_USER'@'%' IDENTIFIED BY '$BDD_USER_PASSWORD';" | mysql
 
-/etc/init.d/mariadb start
+# PRIVILGES FOR ROOT AND USER FOR ALL IP ADRESS #
+echo "GRANT ALL PRIVILEGES ON *.* TO '$BDD_USER'@'%' IDENTIFIED BY '$BDD_USER_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$BDD_ROOT_PASSWORD';" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
 
-#Check if the database exists
+# CREAT WORDPRESS DATABASE #
+echo "CREATE DATABASE $BDD_NAME;" | mysql
 
-if [ -d "/var/lib/mysql/$db1_name" ]
-then 
 
-	echo "Database already exists"
-else
+kill $(cat /var/run/mysqld/mysqld.pid)
 
-# Set root option so that connexion without root password is not possible
-
-mysql_secure_installation << _EOF_
-
-Y
-root4life
-root4life
-Y
-n
-Y
-Y
-_EOF_
-
-#Add a root user on 127.0.0.1 to allow remote connexion 
-#Flush privileges allow to your sql tables to be updated automatically when you modify it
-#mysql -uroot launch mysql command line client
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$WP_ADMIN_PWD'; FLUSH PRIVILEGES;" | mysql -uroot
-
-#Create database and user in the database for wordpress
-
-echo "CREATE DATABASE IF NOT EXISTS $db1_name; GRANT ALL ON $db1_name.* TO '$WP_ADMIN_USR'@'%' IDENTIFIED BY '$WP_ADMIN_PWD'; FLUSH PRIVILEGES;" | mysql -u root
-
-#Import database in the mysql command line
-mysql -uroot -p$WP_ADMIN_PWD $db1_name < /usr/local/bin/wordpress.sql
-
-fi
-
-/etc/init.d/mariadb stop
-
-exec "$@"
+mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,57 +1,45 @@
 #!/bin/bash
 
+# Load environment variables from .env file
+source .env
 
+# Create directory for WordPress installation
+mkdir -p /var/www/html
 
-# create directory to use in nginx container later and also to setup the wordpress conf
-mkdir /var/www/
-mkdir /var/www/html
-
+# Navigate to the WordPress directory
 cd /var/www/html
 
-
+# Remove existing files
 rm -rf *
 
+# Download and install WP CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
 chmod +x wp-cli.phar 
-
 mv wp-cli.phar /usr/local/bin/wp
 
-
+# Download WordPress core files
 wp core download --allow-root
 
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# Create wp-config.php from sample configuration
+wp config create --dbname="$BDD_NAME" --dbuser="$BDD_USER" --dbpass="$BDD_USER_PASSWORD" --dbhost="$BDD_HOST" --skip-check --allow-root
 
-mv /wp-config.php /var/www/html/wp-config.php
+# Install WordPress
+wp core install --url="$DOMAIN_NAME/" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --allow-root
 
+# Create a new user
+wp user create "$WP_USER" "$WP_USER_EMAIL" --role="$WP_USER_ROLE" --user_pass="$WP_USER_PASSWORD" --allow-root
 
-sed -i -r "s/db1/$db_name/1"   wp-config.php
-sed -i -r "s/user/$db_user/1"  wp-config.php
-sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
-
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-
-
-
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-
+# Install Astra theme and activate it
 wp theme install astra --activate --allow-root
 
-
-wp plugin install redis-cache --activate --allow-root
-
+# Update all plugins
 wp plugin update --all --allow-root
 
-
- 
+# Replace the PHP-FPM socket with port 9000 in the FPM pool configuration
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
-mkdir /run/php
+# Create directory for PHP-FPM socket
+mkdir -p /run/php
 
-
-
-wp redis enable --allow-root
-
+# Start PHP-FPM
 /usr/sbin/php-fpm7.3 -F
